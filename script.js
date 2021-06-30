18763c95501e89ea6789c759821334f55f3038e7
wildannugraha99 <mwnwildan99@gmail.com>
2021-06-30 18:35:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,236 @@
+
+//------------------------------PREMIERE LEAGUE SECTION-------------------------------------------------------------------------//
+$.ajax({
+	headers: { 'X-Auth-Token': 'c5fd253b8cc74963ad9baae1f724475a' },
+	type: "GET",
+	url: 'https://api.football-data.org/v2/competitions/2021/teams',
+	data: "data",
+	dataType: "json",
+	success: function (response) {
+		console.log('Premiere League',response)
+
+		const seasons = document.querySelector('.seasons-premiere-league')
+		seasons.innerText = 'Seasons: '+ response.season.startDate + ' / '+ response.season.endDate 
+
+		$.each(response.teams, function(i,data){
+			const logoTeam = document.createElement('div')
+			logoTeam.innerHTML =`<img src="${data.crestUrl}" alt="logo teams" class="logo-team">`
+
+			const nameTeam = document.createElement('h6')
+			nameTeam.innerText = data.name
+
+			const tla = document.createElement('p')
+			tla.innerText = '('+data.tla+')'
+
+			const cardsBody = document.createElement('div')
+			cardsBody.classList.add('cards-body')
+			cardsBody.append(logoTeam,nameTeam,tla)
+
+			const btnTeamDetail = document.createElement('div')
+			btnTeamDetail.innerHTML =`<button class="btn-teamDetail" data-id="${data.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">
+			Team Info</button>`
+			
+			const btnSeeSquad = document.createElement('div')
+			btnSeeSquad.innerHTML =`<button class="btn-seeSquad" data-id="" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+			See Squads</button>`
+
+			const CardsFooter = document.createElement('div')
+			CardsFooter.classList.add('cards-footer')
+			CardsFooter.append(btnTeamDetail,btnSeeSquad)
+
+			const cardTeam = document.createElement('div')
+			cardTeam.classList.add('card-team')
+			cardTeam.append(cardsBody,CardsFooter)
+
+			const innerTeamsSection = document.querySelector('.inner-teams-section-1')
+			innerTeamsSection.append(cardTeam)
+			
+			const innerTeams = document.querySelector('.Premiere-league-section')
+			innerTeams.append(innerTeamsSection)
+
+			const container = document.querySelector('.root-teams')
+			container.append(innerTeams)
+		})
+	}
+});
+//--------------------------------------------------------------------------------------------------------------------------//
+
+
+//------------------------------SERIE A ITALY SECTION--------------------------------------------------------------------------//
+
+$.ajax({
+	headers: { 'X-Auth-Token': 'c5fd253b8cc74963ad9baae1f724475a' },
+	type: "GET",
+	url: 'https://api.football-data.org/v2/competitions/2019/teams',
+	data: "data",
+	dataType: "json",
+	success: function (response) {
+		console.log('Serie A', response)
+
+		const seasons = document.querySelector('.seasons-serie-a')
+		seasons.innerText = 'Seasons: '+ response.season.startDate + ' / '+ response.season.endDate 
+		
+		$.each(response.teams, function(i,data){
+			const logoTeam = document.createElement('div')
+			logoTeam.innerHTML =`<img src="${data.crestUrl}" alt="logo teams" class="logo-team">`
+
+			const nameTeam = document.createElement('h6')
+			nameTeam.innerText = data.name
+			
+			const tla = document.createElement('p')
+			tla.innerText = '('+data.tla+')'
+
+			const cardsBody = document.createElement('div')
+			cardsBody.classList.add('cards-body')
+			cardsBody.append(logoTeam,nameTeam,tla)
+
+			const btnTeamDetail = document.createElement('div')
+			btnTeamDetail.innerHTML =`<button class="btn-teamDetail" data-id="${data.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">
+			Team Info</button>`
+			
+			const btnSeeSquad = document.createElement('div')
+			btnSeeSquad.innerHTML =`<button class="btn-seeSquad" data-id="" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+			See Squads</button>`
+
+			const CardsFooter = document.createElement('div')
+			CardsFooter.classList.add('cards-footer')
+			CardsFooter.append(btnTeamDetail,btnSeeSquad)
+
+			const cardTeam = document.createElement('div')
+			cardTeam.classList.add('card-team')
+			cardTeam.append(cardsBody,CardsFooter)
+
+			const innerTeamsSection = document.querySelector('.inner-teams-section-0')
+			innerTeamsSection.append(cardTeam)
+
+			const innerTeams = document.querySelector('.Serie-A-section')
+			innerTeams.append(innerTeamsSection)
+
+			const container = document.querySelector('.root-teams')
+			container.append(innerTeams)
+			
+		})
+	}
+});
+
+//----------------------------------------------------------------------------------------------------------//
+
+
+//----------------------BUNDESLIGA SECTION-----------------------------------------------------------------//
+
+$.ajax({
+	headers: { 'X-Auth-Token': 'c5fd253b8cc74963ad9baae1f724475a' },
+	type: "GET",
+	url: "https://api.football-data.org/v2/competitions/2002/teams",
+	data: "data",
+	dataType: "json",
+	success: function (response) {
+		console.log('Bundesliga', response)
+
+		const seasons = document.querySelector('.seasons-bundesliga')
+		seasons.innerText = 'Seasons: '+ response.season.startDate + ' / '+ response.season.endDate
+
+		$.each(response.teams, function(i,data){
+			const logoTeam = document.createElement('div')
+			logoTeam.innerHTML =`<img src="${data.crestUrl}" alt="logo teams" class="logo-team">`
+
+			const nameTeam = document.createElement('h6')
+			nameTeam.innerText = data.name
+			
+			const tla = document.createElement('p')
+			tla.innerText = '('+data.tla+')'
+
+			const cardsBody = document.createElement('div')
+			cardsBody.classList.add('cards-body')
+			cardsBody.append(logoTeam,nameTeam,tla)
+
+			const btnTeamDetail = document.createElement('div')
+			btnTeamDetail.innerHTML =`<button class="btn-teamDetail" data-id="${data.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">
+			Team Info</button>`
+			
+			const btnSeeSquad = document.createElement('div')
+			btnSeeSquad.innerHTML =`<button class="btn-SeeSquad" data-id="${data.id}" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+			See Squads</button>`
+
+			const CardsFooter = document.createElement('div')
+			CardsFooter.classList.add('cards-footer')
+			CardsFooter.append(btnTeamDetail,btnSeeSquad)
+
+			const cardTeam = document.createElement('div')
+			cardTeam.classList.add('card-team')
+			cardTeam.append(cardsBody,CardsFooter)
+
+			const innerTeamsSection = document.querySelector('.inner-teams-section-2')
+			innerTeamsSection.append(cardTeam)
+
+			const innerTeams = document.querySelector('.Bundesliga-section')
+			innerTeams.append(innerTeamsSection)
+
+			const container = document.querySelector('.root-teams')
+			container.append(innerTeams)
+			
+		})
+	}
+});
+//----------------------------------------------------------------------------------------------------------------//
+
+$(".main-content").on('click', ".btn-teamDetail", function () {
+	
+	$.ajax({
+		headers: { 'X-Auth-Token': 'c5fd253b8cc74963ad9baae1f724475a' },
+		type: "GET",
+		url: 'https://api.football-data.org/v2/teams/' + $(this).data('id'),
+		dataType: "json",
+		success: function (response) {
+			console.log('Team Detail', response)
+			$('.modal-body').html(`
+				<div class="modalLogoTeam">
+					<img src="${response.crestUrl}">
+					
+				</div>
+
+				<div class="modalInfoTeam">
+					<h4>${response.name}</h4>
+					<p>(${response.tla})</p>
+					<p><b>Founded: </b>${response.founded}</p>
+					<p><b>Club color: </b>${response.clubColors}</p>
+					<p><b>Venue: </b>${response.venue}</p>
+					<p><b>Phone: </b>${response.phone}</p>
+					<p><b>Website: </b><a href="${response.website}" target="_blank">${response.website}</a></p>
+					<p><b>Address: </b>${response.address}</p>
+
+					<p>${response.squad}</p>
+				</div>
+			`)
+
+		}
+	});
+})
+
+//-----------------------------------------------------------------------------------------------------------------//
+
+
+
+//-----------------------------------------------------------------------------------------------------------------//
+$(document).ready(function(){
+	$("a").on('click', function(event) {
+		 
+	 if (this.hash !== "") {
+		 event.preventDefault();
+		 let hash = this.hash;
+		 
+		 $('html, body').animate({
+		   scrollTop: $(hash).offset().top
+		 }, 1000, function(){
+		 window.location.hash = hash;
+		 });
+	   } 
+	 });
+   });
+
+
+
+
+
+
+ 
---
+++ standings-bundesliga.js
@@ -0,0 +1,68 @@
+$.ajax({
+	headers: { 'X-Auth-Token': 'c5fd253b8cc74963ad9baae1f724475a' },
+	type: "GET",
+	url: "https://api.football-data.org/v2/competitions/2002/standings",
+	dataType: "json",
+	success: function (response) {
+		console.log('Standings', response)
+
+        const competitionName = document.createElement('h5')
+        competitionName.innerText=response.competition.name
+
+        const seasons = document.createElement('p')
+        seasons.innerText= 'Season: '+response.season.startDate+' / '+
+        response.season.endDate
+
+       
+        const matchDay = document.createElement('p')
+        matchDay.innerText= 'Match day: '+ response.season.currentMatchday
+
+        const textTitle = document.querySelector('.text-title')
+        textTitle.append(competitionName,seasons,matchDay)
+        
+        const container = document.querySelector('.title')
+        container.append(textTitle)
+
+		$.each(response.standings, function (i,data) { 
+          for(i = 0; i <=20; i++){
+            const posisi = document.createElement('td')
+            posisi.classList.add('posisi-team')
+            posisi.innerText=data.table[i].position
+            
+            const nameTeam = document.createElement('td')
+            nameTeam.classList.add('name-team')
+            nameTeam.innerHTML=`<span><img src="${data.table[i].team.crestUrl}"  class="logo"></span> `+ data.table[i].team.name
+
+            const poin = document.createElement('td')
+            poin.classList.add('poin-team')
+            poin.innerText=data.table[i].points
+
+            const win = document.createElement('td')
+            win.classList.add('win-team')
+            win.innerText=data.table[i].won
+
+            const draw = document.createElement('td')
+            draw.classList.add('draw-team')
+            draw.innerText=data.table[i].draw
+
+            const lose = document.createElement('td')
+            lose.classList.add('lose-team')
+            lose.innerText=data.table[i].lost
+
+
+            const trBody = document.createElement('tr')
+            trBody.classList.add('tr-body')
+            trBody.append(posisi,nameTeam,poin,win,draw,lose)
+
+            const body = document.querySelector('tbody')
+            body.append(trBody)
+
+            const table = document.querySelector('.table')
+            table.append(body)
+          }
+        });
+
+       
+	}
+    
+});
